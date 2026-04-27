2d691e810837e73013b53e285e7f5ab9936d8c5f
Paulo H. C. de Oliveira <oliveira.phc@gmail.com>
2026-04-27 15:09:29 +0100

    chore: install sinon

--- tests/test-helper.ts
+++ tests/test-helper.ts
@@ -1,9 +1,10 @@
+import { setApplication } from '@ember/test-helpers';
 import Application from 'condition-ui-editor/app';
 import config from 'condition-ui-editor/config/environment';
-import * as QUnit from 'qunit';
-import { setApplication } from '@ember/test-helpers';
-import { setup } from 'qunit-dom';
 import { start as qunitStart, setupEmberOnerrorValidation } from 'ember-qunit';
+import setupSinon from 'ember-sinon-qunit';
+import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
 
 export function start() {
   setApplication(Application.create(config.APP));
@@ -11,5 +12,6 @@ export function start() {
   setup(QUnit.assert);
   setupEmberOnerrorValidation();
 
+  setupSinon();
   qunitStart();
 }
