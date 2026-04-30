a3c9b53baef8368a29c877111a2c840225d299c0
Paulo H. C. de Oliveira <oliveira.phc@gmail.com>
2026-04-30 12:00:45 +0100

    chore: install frontile and tailwind

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -1,3 +1,23 @@
 export default {
   extends: ['stylelint-config-standard'],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'plugin',
+          'theme',
+          'source',
+          'utility',
+          'variant',
+          'custom-variant',
+          'config',
+          'tailwind',
+          'apply',
+          'layer',
+          'source',
+        ],
+      },
+    ],
+  },
 };
--- ember-cli-build.mjs
+++ ember-cli-build.mjs
@@ -1,11 +1,22 @@
-import EmberApp from 'ember-cli/lib/broccoli/ember-app.js';
 import { compatBuild } from '@embroider/compat';
+import EmberApp from 'ember-cli/lib/broccoli/ember-app.js';
 
 export default async function (defaults) {
   const { buildOnce } = await import('@embroider/vite');
 
   const app = new EmberApp(defaults, {
-    // Add options here
+    packagerOptions: {
+      webpackConfig: {
+        module: {
+          rules: [
+            {
+              test: /\.css$/i,
+              use: ['postcss-loader'],
+            },
+          ],
+        },
+      },
+    },
   });
 
   return compatBuild(app, buildOnce);
